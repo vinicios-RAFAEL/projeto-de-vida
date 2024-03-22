3b6eb080f63a94e6534d15f4854a93190d96bfcb
vinicios-RAFAEL <114488523+vinicios-RAFAEL@users.noreply.github.com>
2024-03-22 08:20:42 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2020-10-05T00:00:00");
+const tempoObjetivo1 = new Date("2024-03-22T00:08:02");
 const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
 const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
 const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
@@ -43,12 +43,11 @@ function calculaTempo(tempoObjetivo) {
 }
 
 function atualizaCronometro(){
-    document.getElementById("dias0").textContent = calculaTempo(tempos[1])[0];
-    document.getElementById("horas0").textContent = calculaTempo(tempos[1])[1];
-    document.getElementById("min0").textContent = calculaTempo(tempos[1])[2];
-    document.getElementById("seg1").textContent = calculaTempo(tempos[1])[3];
-
     for (let i=0; i<contadores.length;i++){
+        document.getElementById("dias"+i).textContent = calculaTempo(tempos[i])[0];
+        document.getElementById("horas0"+i).textContent = calculaTempo(tempos[i])[1];
+        document.getElementById("min0"+i).textContent = calculaTempo(tempos[i])[2];
+        document.getElementById("seg1"+i).textContent = calculaTempo(tempos[i])[3];
        // contadores[i].textContent = calculaTempo(tempos[i]);   
     }
 }
